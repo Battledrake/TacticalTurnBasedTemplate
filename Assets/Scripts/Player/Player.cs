a7b2415c4b01d18ca5b38716f01cf6fe2bcc478a
BattleDrake <battledrake@gmail.com>
2024-05-07 22:09:09 -0700

    Prep for Index Visuals
    Created prefab and tested spawn performance.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,10 +6,19 @@ namespace BattleDrakeCreations.TTBTk
 {
     public class Player : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] private GameObject _textObjectToSpam;
+
         void Start()
         {
-
+            for(int i = 0; i < 50; i++)
+            {
+                for(int j = 0; j < 50; j++)
+                {
+                    Vector3 spawnPosition = new Vector3(j * 2, 4, i * 2);
+                    Quaternion spawnRotation = Quaternion.Euler(90f, 0f, 0f);
+                    Instantiate(_textObjectToSpam, spawnPosition, spawnRotation, this.transform);
+                }
+            }
         }
 
         // Update is called once per frame
